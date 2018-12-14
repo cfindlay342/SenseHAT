12888ed445bf04100f3c650bbeee28d707818d49
Caden <cfindlay342@lsr7.net>
2018-12-14 08:12:48 -0600

    ball moves

--- pong_Caden.py
+++ pong_Caden.py
@@ -4,6 +4,10 @@ sense = SenseHat()
 
 white = (255,255,255)
 red = (255,0,0)
+purple = (255,0,255)
+ball_position = [3,3]
+ball_velocity = [1,1]
+sense.set_pixel (0,0,purple)
        
 
 bat_y = 4
@@ -14,13 +18,11 @@ def draw_bat():
     sense.set_pixel(0, bat_y - 1, white)
     
 def draw_ball():
-    sense.set_pixel(ball_position[0],ball_position[1], red)
+    sense.set_pixel(ball_position[0], ball_position[1], red)
     ball_position[0] += ball_velocity[0]
-    if ball_position == 7:
-        ball_velocity[0] =- ball_velocity[0]
-
-draw_bat()
-draw_ball()
+    if ball_position[0] == 7 or ball_position[0] == 0:
+        ball_velocity[0] = -ball_velocity[0]
+    ball_position[1] += ball_velocity[0]
 
 sense.clear(0,0,0)
 
@@ -37,7 +39,12 @@ def move_down(event):
 
 while True:
     draw_bat()
-    draw_ball()
     sense.stick.direction_up = move_up
     sense.stick.direction_down = move_down
-    
+    sleep(0.25)
+    sense.clear(0, 0, 0)
+    draw_ball()
+    sense.set_pixel (0,0,purple)
+       
+
+
